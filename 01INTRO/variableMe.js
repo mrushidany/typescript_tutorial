6a6051404fd076534fae4ef88044004241aa15d9
Dany Mrushi <mrushidany@gmail.com>
2023-11-20 17:56:27 +0300

    Exploring typescript inference

--- 01INTRO/variableMe.js
+++ 01INTRO/variableMe.js
@@ -1,2 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "Hello Daniel";
+greetings.toLowerCase();
 console.log(greetings);
+// number
+var userId = 3344366.54;
+userId.toFixed();
+// boolean
+var isLoggedIn = true;
--- 01INTRO/variableMe.ts
+++ 01INTRO/variableMe.ts
@@ -1,5 +1,20 @@
 let greetings: string  = "Hello Daniel";
 
+greetings.toLowerCase()
+
 console.log(greetings)
+
+// number
+
+let userId = 3344366.54
+userId.toFixed()
+
+// boolean
+
+let isLoggedIn: boolean = true
+
+
+
+
 
 export {}
