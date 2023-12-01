fc1a00440b2e68c68c177325ef026e4721e94e66
Dany Mrushi <mrushidany@gmail.com>
2023-12-01 16:28:19 +0300

    Do not use any

--- 01INTRO/variableMe.ts
+++ 01INTRO/variableMe.ts
@@ -5,16 +5,19 @@ greetings.toLowerCase()
 console.log(greetings)
 
 // number
-
 let userId = 3344366.54
 userId.toFixed()
 
 // boolean
-
 let isLoggedIn: boolean = true
 
+// any
+let hero: string;
 
+function getHero(){
+    return 'thor'
+}
 
-
+hero = getHero()
 
 export {}
