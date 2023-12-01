c08c3444238c88972942ac9a8cedda5beedf5d95
Dany Mrushi <mrushidany@gmail.com>
2023-12-01 16:42:15 +0300

    Basic functions

---
+++ 02BASICS/myfunctions.ts
@@ -0,0 +1,20 @@
+function addTwo(num: number) {
+    return num + 2;
+}
+
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean){}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+addTwo(5)
+
+getUpper("4")
+
+signUpUser("Daniel", "mrushidany@gmail.com", false)
+loginUser("Daniel", "mrushidany@gmail.com")
+
+export{}
